5eb0d68abd67b25df834b346061f54dcc9fd798d
huongpv <huongpv@rikkeisoft.com>
2018-12-24 09:41:45 +0700

    textview with tableview and tableviewcell in xib

--- Test/Test/AppDelegate.swift
+++ Test/Test/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         window = UIWindow()
-        let testVC = TestVC()
+        let testVC = Test2VC(nibName: "Test2VC", bundle: nil)
         window?.rootViewController = testVC
         window?.makeKeyAndVisible()
         
---
+++ Test/Test/ViewControllers/Test2VC.swift
@@ -0,0 +1,45 @@
+//
+//  Test2VC.swift
+//  Test
+//
+//  Created by huongpv on 12/24/18.
+//  Copyright © 2018 huongpv. All rights reserved.
+//
+
+import UIKit
+
+class Test2VC: UIViewController {
+    
+    let identifier = "Cell"
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.register(UINib(nibName: "TestCell", bundle: nil), forCellReuseIdentifier: identifier)
+    }
+
+
+
+}
+
+// MARK: - UITableViewDataSource
+extension Test2VC: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 2
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath) as! TestCell
+        cell.str = "More ways to shop: Visit an Apple Store, call 1-800-MY-APPLE, or find a reseller."
+        
+        return cell
+    }
+}
