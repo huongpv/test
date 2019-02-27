718d278c16b0b20484412f4493e67fc452aea433
huongpv <huongpv@rikkeisoft.com>
2019-02-27 16:25:50 +0700

    Login Like ELSA Speak

--- Test/Test/AppDelegate.swift
+++ Test/Test/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         window = UIWindow()
-        let testVC = Test2VC(nibName: "Test2VC", bundle: nil)
+        let testVC = TestVC()
         let navController = UINavigationController(rootViewController: testVC)
         window?.rootViewController = navController
         window?.makeKeyAndVisible()
--- Test/Test/ViewControllers/TestVC.swift
+++ Test/Test/ViewControllers/TestVC.swift
@@ -8,33 +8,73 @@
 
 import UIKit
 
-class TestVC: UITableViewController {
+class TestVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var tableView: UITableView!
     
     let identifier = "Cell"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        tableView.register(UINib(nibName: "TestCell", bundle: nil), forCellReuseIdentifier: identifier)
+        
+        navigationItem.title = "Hồ sơ"
+        
+        setUpdateTableView()
+        
+        let backgroundImage = UIImage(named: "wallpaper.pnp")
+        view.backgroundColor = UIColor(patternImage: backgroundImage ?? UIImage())
+    }
+    
+    private func setUpdateTableView() {
+        tableView = UITableView(frame: view.frame)
+        view.addSubview(tableView)
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.backgroundColor = .clear
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: identifier)
+        tableView.registerNibCellFor(type: FooterCell.self)
+        tableView.separatorStyle = .singleLine
+        tableView.separatorInset = UIEdgeInsets(top: 0, left: 15, bottom: 0, right: 15)
+        tableView.tableFooterView = UIView()
+        
+        // headerView
+        let frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 267)
+        let headerView = HeaderView(frame: frame)
+        tableView.tableHeaderView = headerView
     }
 
     // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
+    func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 2
+        return 3
     }
 
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath) as! TestCell
-        cell.str = "More ways to shop: Visit an Apple Store, call 1-800-MY-APPLE, or find a reseller."
-
-        return cell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.row == 2 {
+            let cell = tableView.reusableCell(type: FooterCell.self)!
+            cell.contentView.backgroundColor = .clear
+            cell.backgroundColor = .clear
+            cell.separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 999)
+            cell.selectionStyle = .none
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+            cell.backgroundColor = .clear
+            cell.textLabel?.textColor = .white
+            cell.accessoryType = .disclosureIndicator
+            cell.selectionStyle = .none
+            cell.textLabel?.text = "ABC"
+            return cell
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("ABC")
     }
 }
