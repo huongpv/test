910367da93dcd5eb48b4c9d3b83760e0da48fc13
huongpv <huongpv@rikkeisoft.com>
2019-04-26 14:45:54 +0700

    update icon plus

--- Test/Test/ViewControllers/MyPage/MyPageVC.swift
+++ Test/Test/ViewControllers/MyPage/MyPageVC.swift
@@ -18,7 +18,7 @@ class MyPageVC: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationController?.navigationBar.setGradientBackgroundNav(startColor: .green, endColor: .blue, gradientDirection: .leftToRight)
+        navigationController?.navigationBar.setGradientBackgroundNav(startColor: #colorLiteral(red: 0.09803921569, green: 0.568627451, blue: 0.9215686275, alpha: 1), endColor: #colorLiteral(red: 0.1764705882, green: 0.631372549, blue: 0.9725490196, alpha: 1), gradientDirection: .leftToRight)
         
         navigationItem.title = "Cài đặt"
         
